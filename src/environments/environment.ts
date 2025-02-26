17619f776776bfc06cc054e63fc8d8c3b91ecad9
Vinicius <167225290+VineJPS@users.noreply.github.com>
2025-02-26 19:36:31 +0000

    aula 19/02/2025

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
   firebaseConfig: {
-    apiKey: "AIzaSyCGdwCWx_8o2hYEmLBhK6xQzCMp5h12gzk",
-    authDomain: "aula-3e82a.firebaseapp.com",
-    projectId: "aula-3e82a",
-    storageBucket: "aula-3e82a.firebasestorage.app",
-    messagingSenderId: "258284807710",
-    appId: "1:258284807710:web:72b06b67cea8d15cd6b00f",
-    measurementId: "G-X8L8GS7897"
+    apiKey: "AIzaSyD8jz4CMLah7lBoEENiqOaASQ5zkg7y3pY",
+    authDomain: "aula-998ed.firebaseapp.com",
+    projectId: "aula-998ed",
+    storageBucket: "aula-998ed.firebasestorage.app",
+    messagingSenderId: "553814414266",
+    appId: "1:553814414266:web:5857d626088cb573358383",
+    measurementId: "G-50MQHQ89MV"
   },
   production: false
 };
