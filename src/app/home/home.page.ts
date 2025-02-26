d64e27cdc939cededa2a6a85fc78f1f208e37ff8
VineJPS <vinicius108sousa@gmail.com>
2025-02-26 18:14:25 -0300

    Aula de Array

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,18 +11,27 @@ import { Router } from '@angular/router';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  filter: string = `fields=name,borders,flags`;
+  url: string = `https://restcountries.com/v3.1/all?${this.filter}`; 
+  dados: any = {};
+  is_loading: boolean = false;
 
-  pokemon:any = {
-    nome: null,
-    poder: null
-  };
+  constructor( ){
+    this.is_loading = true;
 
-  constructor( 
-    public crudService: CrudService
-  ){ }
-
-  enviar() {
-    this.crudService.insert(this.pokemon, 'pokemons');
-  }
+    fetch(this.url)
+      .then(dados=>dados.json()) //converte dados
+      .then(dados=> { //retorna
+        console.log(dados);
+        this.dados = dados; //this.dados é o dados declarado la em cima. E o dados é o parametro do .then
+      }) 
+      .catch(_ =>{ //erros inesperados, _ é uma variavel
+        console.log(_)
+      }) 
+      .finally(()=>{
+        console.log("Requisição Finalizada");
+        this.is_loading = false;
+      })
+   }
 
 }
